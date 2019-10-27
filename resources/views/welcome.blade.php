c45a783f0be371120e8bea1164e5bd7055e68185
kaushalya <harshanathwak@gmail.com>
2019-10-28 02:32:27 +0530

    Added REST Api

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+        @import url(//fonts.googleapis.com/earlyaccess/notosanssinhala.css);
+       body{ font-family: 'Noto Sans Sinhala', sans-serif; 
+       }
+       </style>
+    </head>
+    <body>
+    </body>
+</html>
